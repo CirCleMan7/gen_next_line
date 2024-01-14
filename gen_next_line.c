db5114b5252e52d62f80819110f887014f614634
Sakonkiat Rungsaranon <srungsar@z2t13c2.42bangkok.com>
2024-01-14 14:23:19 +0700

    first line

--- gen_next_line.c
+++ gen_next_line.c
@@ -3,12 +3,48 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-int	main()
+char	*gen_next_line(int fd)
 {
 	static char *buffer;
-	int fd = open("eiei.txt", O_RDONLY);
+	char *line;
+	static char *collectline;
+	int i;
+	int j;
 
+	i = 0;
+	j = 0;
 	buffer = malloc(100);
-	read(fd, buffer, 10);
-	printf("%s", buffer);
+	line = malloc(100);
+	collectline = malloc(100);
+	read(fd, buffer, 20);
+
+	while (buffer[i] != 10)
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	line[i++] = 10;
+	while (buffer[i] != 0)
+	{
+		collectline[j] = buffer[i];
+		i++;
+		j++;
+	}
+	printf("collectline = %s\n", collectline);
+	return (line);
+}
+
+int	main()
+{
+	int fd = open("eiei.txt", O_RDONLY);
+	char *str;
+
+	str = gen_next_line(fd);
+	printf("%s", str);
+/*	while (*buffer != 10)
+	{
+		read(fd, buffer, 10);
+		printf("%s", buffer);
+	}*/
+
 }
