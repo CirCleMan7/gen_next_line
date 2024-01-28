74c7e8d9b19624c2b691a9dc702517895a852212
Sakonkiat Rungsaranon <srungsar@z1t5c4.42bangkok.com>
2024-01-28 12:32:24 +0700

    can use it but still error some point

--- gen_next_line.c
+++ gen_next_line.c
@@ -3,48 +3,155 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-char	*gen_next_line(int fd)
+size_t  ft_strlen(const char *s)
 {
-	static char *buffer;
-	char *line;
-	static char *collectline;
-	int i;
-	int j;
+	int             count;
+	char    *str;
 
+	str = (char *)s;
+	count = 0;
+	while (*str)
+	{
+			str++;
+			count++;
+	}
+	return (count);
+}
+
+size_t  ft_strlcat(char *dest, char *src, size_t amount)
+{
+	size_t  len;
+
+	len = ft_strlen(dest) + ft_strlen(src);
+	if (amount <= ft_strlen(dest))
+			return (ft_strlen(src) + amount);
+	while (*dest)
+	{
+			dest++;
+			amount--;
+	}
+	while (amount-- - 1 && *src)
+	{
+			*dest = *src;
+			dest++;
+			src++;
+	}
+	*dest = 0;
+	return (len);
+}
+
+size_t  ft_strlcpy(char *dest, char *src, size_t amount)
+{
+	size_t  lensrc;
+
+	lensrc = ft_strlen(src);
+	while (amount - 1 && amount > 0 && *src)
+	{
+			*dest = *src;
+			dest++;
+			src++;
+			amount--;
+	}
+	if (amount > 0)
+		*dest = 0;
+	return (lensrc);
+}
+
+int	check_newline(char  *str)
+{
+	int	check;
+
+	check = 0;
+	while (*str)
+	{
+		if (*str == '\n')
+		{
+			check = 1;
+			break;
+		}
+		str++;
+	}
+	return (check);
+}
+
+char	*return_str(char *str)
+{
+	char	*collect;
+	char	*buffer;
+	int		i;
+	int		j;
+
+	collect = malloc(100);
+	buffer = malloc(100);
 	i = 0;
 	j = 0;
-	buffer = malloc(100);
-	line = malloc(100);
-	collectline = malloc(100);
-	read(fd, buffer, 20);
+	while (*(str + i) != '\n' && *(str + i))
+	{
+		*(collect + i) = *(str + i);
+		// printf("%c\n", *(str + i));
+		i++;
+	}
+	*(collect + i++) = '\n';
+	ft_strlcpy(buffer, (str + i), ft_strlen(str + i) + 1);
+	ft_strlcpy(str, buffer, ft_strlen(buffer) + 1);
+	// printf("str = %s\n", str);
+	return (collect);
+}
 
-	while (buffer[i] != 10)
+char	*gen_next_line(int fd)
+{
+	static char *str;
+	char *buffer;
+	char *answer;
+	int i;
+
+	i = 1;
+	answer = malloc(100);
+	buffer = malloc(100);
+	if (!str)
+		str = malloc(100);
+	while (i != 0)
 	{
-		line[i] = buffer[i];
-		i++;
+		if (check_newline(str) != 1)
+		{
+			i = read(fd, buffer, 42);
+			// printf("%d\n", i);
+			ft_strlcat(str, buffer, 1000);
+		}
+		if (check_newline(str) == 1)
+		{
+			// printf("%s", str);
+			answer = return_str(str);
+			break;
+		}
 	}
-	line[i++] = 10;
-	while (buffer[i] != 0)
-	{
-		collectline[j] = buffer[i];
-		i++;
-		j++;
-	}
-	printf("collectline = %s\n", collectline);
-	return (line);
+	return (answer);
 }
 
 int	main()
 {
-	int fd = open("eiei.txt", O_RDONLY);
 	char *str;
-
+	int fd = open("eiei.txt", O_RDONLY);
+	printf("%d\n", fd);
 	str = gen_next_line(fd);
 	printf("%s", str);
-/*	while (*buffer != 10)
-	{
-		read(fd, buffer, 10);
-		printf("%s", buffer);
-	}*/
-
+	str = gen_next_line(fd);
+	printf("%s", str);
+	str = gen_next_line(fd);
+	printf("%s", str);
+	str = gen_next_line(fd);
+	printf("%s", str);
+		str = gen_next_line(fd);
+	printf("%s", str);
+		str = gen_next_line(fd);
+	printf("%s", str);
+	str = gen_next_line(fd);
+	printf("%s", str);
+	str = gen_next_line(fd);
+	printf("%s", str);
+	str = gen_next_line(fd);
+	printf("%s", str);
+	// ft_strlcat("Hello", "World", 10);
+	// printf("%d", check_newline("Hel\nlo"));
+	
 }
