2ae582aa5699efff6a5b332b1d852b1831918607
Sakonkiat Rungsaranon <srungsar@z2t2c4.42bangkok.com>
2024-02-04 14:06:15 +0700

    T_T

--- gen_next_line.c
+++ gen_next_line.c
@@ -3,42 +3,21 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-	int             count;
-	char    *str;
+	int		count;
+	char	*str;
 
 	str = (char *)s;
 	count = 0;
 	while (*str)
 	{
-			str++;
-			count++;
+		str++;
+		count++;
 	}
 	return (count);
 }
 
-size_t  ft_strlcat(char *dest, char *src, size_t amount)
-{
-	size_t  len;
-
-	len = ft_strlen(dest) + ft_strlen(src);
-	if (amount <= ft_strlen(dest))
-			return (ft_strlen(src) + amount);
-	while (*dest)
-	{
-			dest++;
-			amount--;
-	}
-	while (amount-- - 1 && *src)
-	{
-			*dest = *src;
-			dest++;
-			src++;
-	}
-	*dest = 0;
-	return (len);
-}
 
 size_t  ft_strlcpy(char *dest, char *src, size_t amount)
 {
@@ -47,14 +26,39 @@ size_t  ft_strlcpy(char *dest, char *src, size_t amount)
 	lensrc = ft_strlen(src);
 	while (amount - 1 && amount > 0 && *src)
 	{
-			*dest = *src;
-			dest++;
-			src++;
-			amount--;
+		*dest = *src;
+		dest++;
+		src++;
+		amount--;
 	}
 	if (amount > 0)
 		*dest = 0;
 	return (lensrc);
+}
+
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+	char    *start;
+	char    *answer;
+
+	answer = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!answer)
+		return (NULL);
+	start = answer;
+	while (*s1)
+	{
+		*answer = *s1;
+		s1++;
+		answer++;
+	}
+	while (*s2)
+	{
+		*answer = *s2;
+		s2++;
+		answer++;
+	}
+	*answer = '\0';
+	return (start);
 }
 
 int	check_newline(char  *str)
@@ -62,6 +66,8 @@ int	check_newline(char  *str)
 	int	check;
 
 	check = 0;
+	if (str == 0)
+		return (0);
 	while (*str)
 	{
 		if (*str == '\n')
@@ -74,6 +80,21 @@ int	check_newline(char  *str)
 	return (check);
 }
 
+int len_font(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str)
+	{
+		len += 1;
+		if (*str == '\n')
+			break;
+		str++;
+	}
+	return (len);
+}
+
 char	*return_str(char *str)
 {
 	char	*collect;
@@ -81,21 +102,48 @@ char	*return_str(char *str)
 	int		i;
 	int		j;
 
-	collect = malloc(100);
-	buffer = malloc(100);
+	collect = malloc(sizeof(char) * (len_font(str) + 1));
+	// printf("%lu\n", (ft_strlen(str) - ft_strlen(collect) + 1));
 	i = 0;
 	j = 0;
 	while (*(str + i) != '\n' && *(str + i))
 	{
 		*(collect + i) = *(str + i);
+		// printf("%c", *(collect + i));
 		// printf("%c\n", *(str + i));
 		i++;
 	}
+	// printf("\n");
 	*(collect + i++) = '\n';
+	*(collect + i) = '\0';
+	buffer = malloc(sizeof(char) * (ft_strlen(str) - ft_strlen(collect) + 1));
 	ft_strlcpy(buffer, (str + i), ft_strlen(str + i) + 1);
+	free(str);
+	str = malloc(sizeof(char) * (ft_strlen(buffer) + 1));
 	ft_strlcpy(str, buffer, ft_strlen(buffer) + 1);
-	// printf("str = %s\n", str);
+	free(buffer);
+	// printf("collect = %s\n", collect);
 	return (collect);
+}
+
+void	joinStr(char **str, char *buffer)
+{
+	char	*temp;
+
+	printf("ei");
+	// ft_strlcpy(temp, str);
+	// printf("buffer = %s\n", buffer);
+	**str = 0;
+	temp = ft_strjoin(*str, buffer);
+	// printf("temp = %s\n", temp);
+	// if (str)
+	// printf("*str = %p\n", *str);
+	// if ((*str) != NULL)
+		// free(*str);
+	// free(buffer);
+	// *str = temp;
+	// printf("str = %s\n", *str);
+	// printf("str = %s\n", *str);
 }
 
 char	*gen_next_line(int fd)
@@ -103,24 +151,28 @@ char	*gen_next_line(int fd)
 	static char *str;
 	char *buffer;
 	char *answer;
+	int BUFFER_SIZE;
 	int i;
 
 	i = 1;
-	answer = malloc(100);
-	buffer = malloc(100);
-	if (!str)
-		str = malloc(100);
+	BUFFER_SIZE = 2;
+	buffer = malloc(BUFFER_SIZE + 1);
+
 	while (i != 0)
 	{
 		if (check_newline(str) != 1)
 		{
-			i = read(fd, buffer, 42);
-			// printf("%d\n", i);
-			ft_strlcat(str, buffer, 1000);
+			i = read(fd, buffer, BUFFER_SIZE);
+			buffer[i] = '\0';
+			printf("buffer = %s\n", buffer);
+			// printf("e");
+			// printf("buffer = %s\n", buffer);
+			printf("a\n");
+			joinStr(&str, buffer);
 		}
+		// printf("str .. = %s\n", str);
 		if (check_newline(str) == 1)
 		{
-			// printf("%s", str);
 			answer = return_str(str);
 			break;
 		}
@@ -134,23 +186,12 @@ int	main()
 	int fd = open("eiei.txt", O_RDONLY);
 	printf("%d\n", fd);
 	str = gen_next_line(fd);
-	printf("%s", str);
-	str = gen_next_line(fd);
-	printf("%s", str);
-	str = gen_next_line(fd);
-	printf("%s", str);
-	str = gen_next_line(fd);
-	printf("%s", str);
-		str = gen_next_line(fd);
-	printf("%s", str);
-		str = gen_next_line(fd);
-	printf("%s", str);
-	str = gen_next_line(fd);
-	printf("%s", str);
-	str = gen_next_line(fd);
-	printf("%s", str);
-	str = gen_next_line(fd);
-	printf("%s", str);
+	// printf("%s", str);
+	// str = gen_next_line(fd);
+	// printf("%s", str);
+	// str = gen_next_line(fd);
+	// printf("%s", str);
+	// free(str);
 	// ft_strlcat("Hello", "World", 10);
 	// printf("%d", check_newline("Hel\nlo"));
 	
